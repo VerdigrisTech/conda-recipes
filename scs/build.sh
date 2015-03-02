4a31c4539d6bbacd684b51b4242f343261800fb6
Jacques Kvam <jwkvam@gmail.com>
2015-03-02 11:12:48 -0800

    update scs and ecos to git versions with memory leak fixes

--- scs/build.sh
+++ scs/build.sh
@@ -2,5 +2,4 @@
 export LAPACK_INCLUDE_PATH=$LIBRARY_PATH
 export LAPACK_LIB_PATH=$INCLUDE_PATH
 export LAPACK_LIBS='f2c blas lapack'
-$PYTHON setup.py install
-
+cd python && $PYTHON setup.py install
